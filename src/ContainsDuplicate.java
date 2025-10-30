c29a2de7697b321396ab0f41ac8c945c70fbf3c4
Gunasekaran <Gunasekaran.Ragunathan@solverines.com>
2025-10-30 16:14:20 +0530

    contains duplicate problem

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,45 @@
+import java.util.HashMap;
+
+public class ContainsDuplicate {
+/*
+* Given an integer array nums, return true if any value appears more than once
+* in the array, otherwise return false.
+*
+* Example 1:
+
+  Input: nums = [1, 2, 3, 3]
+
+  Output: true
+
+* Example 2:
+
+  Input: nums = [1, 2, 3, 4]
+
+  Output: false
+* ***********************
+*  Question Source: neetcode.io
+*  Approach: Used Hashmap to find the element.
+*  T.C: o(n)
+*  S.C o(n)
+* */
+
+    public static boolean findDuplicate(int[] arr)
+    {
+        if(arr.length == 0)
+        {
+            return false;
+        }
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if(map.containsValue(arr[i])){
+               return  true;
+            }
+
+            map.put(i,arr[i]);
+        }
+        return false;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1,2,3,4};
+
+        System.out.println(ContainsDuplicate.findDuplicate(arr));
+
+    }
+}
